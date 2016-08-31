1663517beefc22a7e6e66b990c88d54bea59aa5c
sderekj <dereksantos1989@gmail.com>
2016-08-31 01:46:27 -0400

    Changes to Loop in Hangman

--- src/nyc/c4q/Main.java
+++ src/nyc/c4q/Main.java
@@ -1,24 +1,49 @@
 package nyc.c4q;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        Hangman hangman = new Hangman();
-        while (hangman.getMisses() < 5){
-            hangman.printCurrentWord();
-            hangman.promptPlayer("Enter a letter: ");
-            hangman.readLetter();
-            hangman.checkLetter();
-            if(hangman.guessedSuccessfully()){
-                break;
+        boolean notDone = true;
+        while (notDone) {
+            Hangman hangman = new Hangman();
+            while (hangman.getMisses() < 5) {
+                hangman.printCurrentWord();
+                hangman.promptPlayer("Enter a letter: ");
+                hangman.readLetter();
+                hangman.checkLetter();
+                if (hangman.guessedSuccessfully()) {
+                    break;
+                }
+                System.out.println(Drawing.get(hangman.getMisses()));
+                System.out.println("Misses -> " + hangman.getMisses());
             }
-            System.out.println(Drawing.get(hangman.getMisses()));
-            System.out.println("Misses -> " +hangman.getMisses());
+            if (hangman.guessedSuccessfully()) {
+                System.out.println("Success");
+            } else {
+                System.out.println("The answer was " + hangman.getSecretWord());
+            }
+            System.out.println("Do you want to play again?"); // prompt("Do you want to play again?");
+            /*************************************/
+            boolean isValidInput;
+            do {
+                Scanner answer = new Scanner(System.in);
+                String userAnswer = answer.next();
+                userAnswer = userAnswer.toLowerCase();
+                if (userAnswer.equals("yes") || userAnswer.equals("y")) {
+                    isValidInput = true;
+                    notDone = true;
+                } else if (userAnswer.equals("no") || userAnswer.equals("n")) {
+                    isValidInput = true; // consider doing the replay of hangman in this part of the loop
+                    notDone = false;
+                } else {
+                    isValidInput = false;
+                    System.out.println("Sorry, I didn't understand that. Could you please try again."); // prompt("Try again.")
+                }
+            } while (!isValidInput);
+            /*************************************/
         }
-        if(hangman.guessedSuccessfully()){
-            System.out.println("Success");
-        } else {
-            System.out.println("The answer was " + hangman.getSecretWord());
-        }
+
     }
 }
